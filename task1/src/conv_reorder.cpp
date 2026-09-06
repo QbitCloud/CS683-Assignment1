f76c50e2846cf841b2a0a807f4d51106708b9cce
Deva <45530397+deva-narayana-babu@users.noreply.github.com>
2026-09-06 22:06:13 +0530

    changed loop order to planned order
    same as title

--- task1/src/conv_reorder.cpp
+++ task1/src/conv_reorder.cpp
@@ -9,20 +9,24 @@ void conv_reorder(const float *in, float *out, const float *ker, int H, int W,
   const int p = K / 2;
   const int in_stride = W + 2 * p;
 
-  std::memset(out, 0, sizeof(float) * H * W);
-
-  for (int ky = 0; ky < K; ++ky)
+  for (int oy = 0; oy < H; ++oy)
   {
-    for (int kx = 0; kx < K; ++kx)
+    //h
+    float *out_row = out + oy * W;
+    std::memset(out_row, 0, sizeof(float) * W);
+
+    for (int ky = 0; ky < K; ++ky)
     {
-      const float kval = ker[ky * K + kx]; // loop-invariant, hoisted
-      for (int oy = 0; oy < H; ++oy)
+      const float *in_row = in + (oy + ky) * in_stride;
+      const float *ker_row = ker + ky * K;
+
+      for (int kx = 0; kx < K; ++kx)
       {
-        const float *in_row = in + (oy + ky) * in_stride + kx;
-        float *out_row = out + oy * W;
+        // "hoisted"
+        const float kval = ker_row[kx]; 
         for (int ox = 0; ox < W; ++ox)
         {
-          out_row[ox] += in_row[ox] * kval; // unit-stride, no cross-lane dependency
+          out_row[ox] += in_row[ox + kx] * kval; 
         }
       }
     }
